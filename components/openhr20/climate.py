25720f2acf68ad2002cfa4661d6f0b0144922397
Daniel Lemper <ich@dlemper.de>
2022-09-26 14:05:54 +0200

    try fixing

--- components/openhr20/climate.py
+++ components/openhr20/climate.py
@@ -10,14 +10,15 @@ DEPENDENCIES = ["uart"]
 openhr20_ns = cg.esphome_ns.namespace("openhr20")
 OpenHR20Climate = openhr20_ns.class_("OpenHR20Climate", climate.Climate, cg.Component, uart.UARTDevice)
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend(
+CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(OpenHR20Climate),
     }
-)
+).extend(climate.CLIMATE_SCHEMA).extend(cv.Component).extend(uart.UART_DEVICE_SCHEMA)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
+    await uart.register_uart_device(var, config)
